d2b8b826859c2a87970ca0aecdf01bd60ba927b9
macak.nicolas@gmail.com <xmacakn@stuba.sk>
2021-10-12 21:32:04 +0200

    vik vik je cool

---
+++ P1U1/src/main/java/com/asos/p1u1/MessageConsumerIfc.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.asos.p1u1;
+
+/**
+ *
+ * @author edu
+ */
+public interface MessageConsumerIfc {
+    void putMessage(String msg);
+}
---
+++ P1U1/src/main/java/com/asos/p1u1/MessageConsumerMock.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.asos.p1u1;
+
+/**
+ *
+ * @author edu
+ */
+public class MessageConsumerMock implements MessageConsumerIfc {
+    @Override
+    public void putMessage(String msg) {
+        System.out.println(msg);
+    }
+}
